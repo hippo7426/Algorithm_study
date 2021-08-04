fefc459b7c948cfce53004c4c13b93fa9f6a6765
SeungJun <hippo7426@gmail.com>
2021-08-04 14:30:29 +0900

    file name(test) changed

---
+++ Test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	cin.tie(NULL);
+	cin.sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cout << i << '\n';
+	}
+}
